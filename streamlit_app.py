9ac310056ecbc63f614fe38d65b452185cabaab3
Sahrul Yudha Farhrezi <78601066+yufaa@users.noreply.github.com>
2024-01-05 06:59:45 +0000

    eror load model

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,7 @@ df_final = pd.read_csv("final_data.csv")
 X = df_final.drop(["Unnamed: 0","target"], axis=1).values
 y = df_final['target'].values
 
-# model = pickle.load(open("model/rf_model.pkl", 'rb'))
-
-# Buka file model pkl dalam mode binary (rb: read binary)
-with open("model/rf_model.pkl", 'rb') as file:
-    # Muat model dari file
-    model = pickle.load(file)
+model = pickle.load(open("model/rf_model.pkl", 'rb'))
 
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
